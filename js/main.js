fce95a372f40c173d397f39725f5495ea2db73a1
Gabriele <amoregabriele.95@gmail.com>
2020-12-02 15:24:35 +0100

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 /*
-Milestone 1: 
+Milestone 1:
 Creare un layout base con una searchbar (una input e un button) 
 in cui possiamo scrivere completamente o parzialmente il nome di un film. 
-Possiamo, cliccando il bottone, cercare sull’API tutti i film 
-che contengono ciò che ha scritto l’utente. 
+Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono 
+ciò che ha scritto l’utente.
 Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori 
 per ogni film trovato: 
-Titolo Titolo Originale Lingua Voto 
-nome repo: vue-boolflix
+Titolo
+Titolo Originale
+Lingua
+Voto
 */
 
 const API_KEY = 'ede7fb5e58667f60723d8fe5658849f4';
@@ -38,14 +40,3 @@ const app = new Vue ({
         }
     }
 })
-
-/*
-axios
-    .get('https://flynn.boolean.careers/exercises/api/array/music')
-    .then(result => {
-            this.disks.push(result.data.response[i]);
-        if(!this.genres.includes(result.data.response[i].genre)){
-            this.genres.push(result.data.response[i].genre)
-        }
-    });
-*/
